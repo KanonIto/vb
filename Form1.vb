4fb29bd3130a3190d6c1a7d976d68b391ff3738c
KanonIto <82356518+KanonIto@users.noreply.github.com>
2021-04-12 14:22:19 +0900

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,60 +1,81 @@
 ﻿Public Class Form1
+
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim q1 As Integer = Me.TextBox1.Text
+        Dim r As New System.Random
 
-        If q1 = 1 Then
-            Dim cg As Graphics = Me.PictureBox1.CreateGraphics
-            Dim jpg1 As Image = Image.FromFile("こどもの国線.jpg")
+        Dim a As Integer = r.Next(1, 4)
 
-            cg.DrawImage(jpg1, 0, 0, jpg1.Width, jpg1.Height)
-
-            Me.TextBox5.Text = "片道７分全３駅！　東急こどもの国線"
-
-
-        Else
-            Me.TextBox4.Text = "Q2 観光地をめぐるのは好きだ"
-
-        End If
-
-
+        TabControl1.SelectedTab = TabControl1.TabPages(a)
 
     End Sub
 
+
+
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim q2 As Integer = Me.TextBox2.Text
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
 
-        If q2 = 1 Then
-            Dim cg As Graphics = Me.PictureBox1.CreateGraphics
-            Dim jpg2 As Image = Image.FromFile("江ノ島電鉄.jpg")
-            cg.DrawImage(jpg2, 0, -50, jpg2.Width, jpg2.Height)
-            Me.TextBox5.Text = "映画やアニメの聖地　江ノ島電鉄"
-
-        Else
-            Me.TextBox4.Text = "Q3 ゆるキャラが好きである"
-
-        End If
+        TabControl1.SelectedTab = TabControl1.TabPages(1 + a)
 
 
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Dim q3 As Integer = Me.TextBox3.Text
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
 
-        If q3 = 1 Then
-            Dim cg As Graphics = Me.PictureBox1.CreateGraphics
-            Dim jpg3 As Image = Image.FromFile("相鉄いずみ野線.jpg")
-            cg.DrawImage(jpg3, 0, 0, jpg3.Width, jpg3.Height)
-            Me.TextBox5.Text = "かわいい猫のゆるキャラがいる　相鉄いずみ野線"
+        TabControl1.SelectedTab = TabControl1.TabPages(2 + a)
+    End Sub
 
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TabControl1.SelectedTab = TabControl1.TabPages(0)
 
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
+
+        TabControl1.SelectedTab = TabControl1.TabPages(4 + a)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
+
+        TabControl1.SelectedTab = TabControl1.TabPages(5 + a)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
+
+        TabControl1.SelectedTab = TabControl1.TabPages(6 + a)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
+
+        If a = 1 Then
+            TabControl1.SelectedTab = TabControl1.TabPages(8)
         Else
-            Me.TextBox4.Text = ""
-            Dim cg As Graphics = Me.PictureBox1.CreateGraphics
-            Dim jpg4 As Image = Image.FromFile("小田急多摩線.jpg")
-            cg.DrawImage(jpg4, 0, -140, jpg4.Width, jpg4.Height)
-            Me.TextBox5.Text = "のんびりしたいそこのあなたに　小田急多摩線"
-
+            TabControl1.SelectedTab = TabControl1.TabPages(9)
         End If
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim r As New System.Random
+        Dim a As Integer = r.Next(1, 4)
+
+        TabControl1.SelectedTab = TabControl1.TabPages(8 - a)
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TabControl1.SelectedTab = TabControl1.TabPages(0)
     End Sub
 End Class
